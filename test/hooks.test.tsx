fbb3822978e2479e86efcf4fac6fb8e1e12f06db
Sam Gwilym <gwilym@me.com>
2020-10-07 14:15:34 +0000

    add more tests for useRemoveWorkspace, useWorkspacePubs, and usePubs

--- test/hooks.test.tsx
+++ test/hooks.test.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { ValidatorEs4, StorageMemory } from 'earthstar';
 import { renderHook, act } from '@testing-library/react-hooks';
-import { EarthstarPeer, useWorkspaces, useAddWorkspace } from '../src';
+import {
+  EarthstarPeer,
+  useWorkspaces,
+  useAddWorkspace,
+  useRemoveWorkspace,
+  useWorkspacePubs,
+  usePubs,
+} from '../src';
 
 const WORKSPACE_ADDR_A = '+testa.a123';
 const WORKSPACE_ADDR_B = '+testb.b234';
@@ -29,7 +36,7 @@ const wrapper = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-test('useWorkspace returns workspaces', () => {
+test('useWorkspace', () => {
   const { result } = renderHook(() => useWorkspaces(), { wrapper });
 
   expect(result.current).toEqual([
@@ -39,7 +46,7 @@ test('useWorkspace returns workspaces', () => {
   ]);
 });
 
-test('useAddWorkspace adds a workspace', () => {
+test('useAddWorkspace ', () => {
   const useTest = () => {
     const add = useAddWorkspace();
     const workspaces = useWorkspaces();
@@ -60,6 +67,7 @@ test('useAddWorkspace adds a workspace', () => {
     '+testd.d789',
   ]);
 
+  // Can't add a workspace twice
   act(() => {
     result.current.add(WORKSPACE_ADDR_A);
   });
@@ -70,4 +78,52 @@ test('useAddWorkspace adds a workspace', () => {
     WORKSPACE_ADDR_C,
     '+testd.d789',
   ]);
+});
+
+test('useRemoveWorkspace', () => {
+  const useTest = () => {
+    const remove = useRemoveWorkspace();
+    const workspaces = useWorkspaces();
+
+    return { remove, workspaces };
+  };
+
+  const { result } = renderHook(() => useTest(), { wrapper });
+
+  act(() => {
+    result.current.remove(WORKSPACE_ADDR_C);
+  });
+
+  expect(result.current.workspaces).toEqual([
+    WORKSPACE_ADDR_A,
+    WORKSPACE_ADDR_B,
+  ]);
+});
+
+test('useWorkspacePubs', () => {
+  const { result } = renderHook(() => useWorkspacePubs(WORKSPACE_ADDR_A), {
+    wrapper,
+  });
+
+  expect(result.current[0]).toEqual([PUB_A]);
+
+  act(() => {
+    result.current[1](prev => [...prev, PUB_B]);
+  });
+
+  expect(result.current[0]).toEqual([PUB_A, PUB_B]);
+});
+
+test('usePubs', () => {
+  const { result } = renderHook(() => usePubs(), {
+    wrapper,
+  });
+
+  expect(result.current[0]).toEqual(pubs);
+
+  act(() => {
+    result.current[1]({ [WORKSPACE_ADDR_A]: [PUB_C] });
+  });
+
+  expect(result.current[0]).toEqual({ [WORKSPACE_ADDR_A]: [PUB_C] });
 });
