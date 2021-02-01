a30a28604f708a404959bce78dd74c5301cf3c9b
Sam Gwilym <gwilym@me.com>
2021-02-01 13:48:42 +0000

    Close tab when removing workspace using WorkspaceManagerPanel

--- src/components/earthbar/WorkspaceManagerPanel.tsx
+++ src/components/earthbar/WorkspaceManagerPanel.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { EarthbarTabPanel } from './Earthbar';
+import { EarthbarContext, EarthbarTabPanel } from './Earthbar';
 import { WorkspaceOptions } from './WorkspaceOptions';
 
 export default function WorkspaceManagerPanel() {
+  const { setActiveIndex, setFocusedIndex } = React.useContext(EarthbarContext);
+
   return (
     <EarthbarTabPanel data-re-workspace-manager-panel>
-      <WorkspaceOptions />
+      <WorkspaceOptions
+        onRemove={() => {
+          setActiveIndex(-1);
+          setFocusedIndex(-1);
+        }}
+      />
     </EarthbarTabPanel>
   );
 }
